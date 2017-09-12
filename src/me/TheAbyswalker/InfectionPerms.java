a88629098fc0d3a77683865a8e5788424cf3e138
TheAbyswalker <samyoung051@gmail.com>
2017-09-12 11:41:54 +1000

    update 1

--- src/me/TheAbyswalker/InfectionPerms.java
+++ src/me/TheAbyswalker/InfectionPerms.java
@@ -1,10 +1,16 @@
 package me.TheAbyswalker;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class InfectionPerms extends JavaPlugin{
+public class InfectionPerms extends JavaPlugin implements Listener{
+	
+	public static InfectionPerms plugin;
 	
 	@Override
-	public void onEnable() {}
+	public void onEnable() {
+		plugin = this;
+		this.getServer().getPluginManager().registerEvents(this, this);
+	}
 	
 }
